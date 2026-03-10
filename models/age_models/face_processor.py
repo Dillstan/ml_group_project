2829013afe443e30da87d1abe9be90120d6d7098
maxklema <maxklema@gmail.com>
2026-03-09 21:43:01 -0400

    minor changes

--- models/age_models/face_processor.py
+++ models/age_models/face_processor.py
@@ -5,7 +5,7 @@ import os
 #process one image
 def process_single_image(file_name):
   image = face_recognition.load_image_file(os.path.join("./datasets/faces/images", file_name))
-  face_locations = face_recognition.face_locations(image, model="cnn")
+  face_locations = face_recognition.face_locations(image, model="hog")
   if len(face_locations) > 0: # face found in image
 
     # Get Cropped Image
@@ -13,7 +13,7 @@ def process_single_image(file_name):
     face_image = image[top:bottom, left:right]
     pil_image = Image.fromarray(face_image)
 
-     # add padding
+    # add padding
     processed_image = ImageOps.pad(
         pil_image,
         (160,160),
