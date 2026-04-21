a2dd28fbff4e99bbf66134d6094eb84476e6a8c7
maxklema <maxklema@gmail.com>
2026-04-21 16:52:08 -0400

    draft ranking algorithmm

--- tmdb_files/media_ranking_alg.py
+++ tmdb_files/media_ranking_alg.py
@@ -6,6 +6,8 @@ import concurrent.futures
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
+from collections import defaultdict
 
 load_dotenv() # load environment variables
 
@@ -27,15 +29,55 @@ def rank_media(year_range, tmdb_ids, num_places):
     # fetch movie credits for each actor
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         results = list(executor.map(fetch_movies, tmdb_ids))
+    # with open("ranking.json", "r") as f:
+    #     results = json.loads(str(f.read()))
 
     # calculate rankings
 
+    rankings_dict = {}
     rankings = []
 
+    # iterate over all movies to map frequencies
+    for movie_credit in results:
+        for movie in movie_credit:
+            
+            if (len(movie["release_date"]) == 0): # exclude in-valid movie dates
+                continue
+            
+            movie_date_split = movie["release_date"].split("-")
+            movie_year = int(movie_date_split[0])
+            movie_month = int(movie_date_split[1])
+            movie_day = int(movie_date_split[2])
+            if (movie_year <= year_range[1] and movie_year >= year_range[0]):
+                movie_details = (movie["title"], date(movie_year, movie_month, movie_day), movie["poster_path"]) # movie title, release date, poster img path
+                rankings_dict[movie_details] = rankings_dict.get(movie_details, 0) + 1 # increase frequency count of movie
 
+    # group movies by frequencies
+    grouped_by_freq = defaultdict(set)
 
-    return rankings
+    for movie, freq in rankings_dict.items():
+        grouped_by_freq[freq].add(movie)
+    
+    grouped_by_freq = dict(grouped_by_freq)
 
+    # for each frequency count, order by closest to median age in tuple
+    median_date = (year_range[0] + year_range[1]) / 2
+    if (median_date % 2 == 0):
+        median_date = date(median_date, 1, 1)
+    else:
+        median_date = date(int(median_date), 7, 2) # middle day of the year
+
+    # for each frequency, append movies in order of them being closest to the median date
+
+    for freq, movie_set in grouped_by_freq.items():
+        freq_rankings = []
+        for movie_tuple in movie_set:
+            age_differences = abs(median_date - movie_tuple[1])
+            freq_rankings.append((age_differences, movie_tuple))
+        freq_rankings.sort()
+        rankings.extend(freq_rankings)
+
+    return [movie[1] for movie in rankings[:5]]
 
 # testing
 year_range = (1980,1990)
@@ -43,6 +85,3 @@ tmdb_ids = [521, 1064, 44735]
 num_places = 5
 
 print(rank_media(year_range, tmdb_ids, num_places))
-# with open("ranking.json", "w") as f:
-#     rankings = rank_media(year_range, tmdb_ids, num_places)
-#     f.write(str(rankings))
