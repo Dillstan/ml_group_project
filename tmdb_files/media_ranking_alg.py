8ca8f153160b6b4fdb0ba46640c4d1048c29f6f5
maxklema <maxklema@gmail.com>
2026-04-21 17:07:22 -0400

    added concurrency and bug fixes

--- tmdb_files/media_ranking_alg.py
+++ tmdb_files/media_ranking_alg.py
@@ -10,6 +10,7 @@ from datetime import date
 from collections import defaultdict
 
 load_dotenv() # load environment variables
+rankings_dict = {}
 
 # make request to get list of movie credits for each actor
 def fetch_movies(tmdb_id):
@@ -24,33 +25,31 @@ def fetch_movies(tmdb_id):
     json_response = json.loads(response.text)
     return json_response["cast"]
 
+def update_movie_frequency(movie_credit):
+    for movie in movie_credit: 
+        if (len(movie["release_date"]) == 0): # exclude in-valid movie dates
+            continue
+        
+        movie_date_split = movie["release_date"].split("-")
+        movie_year = int(movie_date_split[0])
+        movie_month = int(movie_date_split[1])
+        movie_day = int(movie_date_split[2])
+        if (movie_year <= year_range[1] and movie_year >= year_range[0]):
+            movie_details = (movie["title"], date(movie_year, movie_month, movie_day), movie["poster_path"]) # movie title, release date, poster img path
+            rankings_dict[movie_details] = rankings_dict.get(movie_details, 0) + 1 # increase frequency count of movie
+
 def rank_media(year_range, tmdb_ids, num_places):
 
     # fetch movie credits for each actor
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         results = list(executor.map(fetch_movies, tmdb_ids))
-    # with open("ranking.json", "r") as f:
-    #     results = json.loads(str(f.read()))
 
     # calculate rankings
-
-    rankings_dict = {}
     rankings = []
 
     # iterate over all movies to map frequencies
-    for movie_credit in results:
-        for movie in movie_credit:
-            
-            if (len(movie["release_date"]) == 0): # exclude in-valid movie dates
-                continue
-            
-            movie_date_split = movie["release_date"].split("-")
-            movie_year = int(movie_date_split[0])
-            movie_month = int(movie_date_split[1])
-            movie_day = int(movie_date_split[2])
-            if (movie_year <= year_range[1] and movie_year >= year_range[0]):
-                movie_details = (movie["title"], date(movie_year, movie_month, movie_day), movie["poster_path"]) # movie title, release date, poster img path
-                rankings_dict[movie_details] = rankings_dict.get(movie_details, 0) + 1 # increase frequency count of movie
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(update_movie_frequency, results)
 
     # group movies by frequencies
     grouped_by_freq = defaultdict(set)
@@ -61,15 +60,15 @@ def rank_media(year_range, tmdb_ids, num_places):
     grouped_by_freq = dict(grouped_by_freq)
 
     # for each frequency count, order by closest to median age in tuple
-    median_date = (year_range[0] + year_range[1]) / 2
+    median_date = int((year_range[0] + year_range[1]) / 2)
     if (median_date % 2 == 0):
         median_date = date(median_date, 1, 1)
     else:
-        median_date = date(int(median_date), 7, 2) # middle day of the year
+        median_date = date(median_date, 7, 2) # middle day of the year
 
     # for each frequency, append movies in order of them being closest to the median date
 
-    for freq, movie_set in grouped_by_freq.items():
+    for freq, movie_set in sorted(grouped_by_freq.items(), reverse=True):
         freq_rankings = []
         for movie_tuple in movie_set:
             age_differences = abs(median_date - movie_tuple[1])
@@ -77,10 +76,10 @@ def rank_media(year_range, tmdb_ids, num_places):
         freq_rankings.sort()
         rankings.extend(freq_rankings)
 
-    return [movie[1] for movie in rankings[:5]]
+    return [movie[1] for movie in rankings[:num_places]]
 
 # testing
-year_range = (1980,1990)
+year_range = (1980,2000)
 tmdb_ids = [521, 1064, 44735]
 num_places = 5
 
